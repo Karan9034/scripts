81bce8e1a735ff23df12a82278849118692cb248
Karan Agrawal <karan.agr9034@gmail.com>
2024-07-24 14:27:44 +0530

    Update setup.sh

--- codespaces/setup.sh
+++ codespaces/setup.sh
@@ -10,10 +10,12 @@
 # Can use hardhat cli using npx
 # npm i -g hardhat
 
-#Install Foundry
+#Install Foundry & Huff
 curl -L https://foundry.paradigm.xyz | bash
+curl -L get.huff.sh | bash
 source ~/.bashrc
 foundryup
+huffup
 
 # Install Slither
 python3 -m pip install slither-analyzer
